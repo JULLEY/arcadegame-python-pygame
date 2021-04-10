f671243f31e26cb274e83ea21b392f38230048f4
woong <ung91@yahoo.com>
2021-04-11 00:37:33 +0900

    pygame basic 끝

--- pygame_basic/6_collision.py
+++ pygame_basic/6_collision.py
@@ -14,10 +14,10 @@ pygame.display.set_caption("웅겹살의 오락실게임") # 화면설정 screen
 clock = pygame.time.Clock()
 
 # 배경 이미지 불러오기
-background = pygame.image.load("C:/PythonWorkspace/pygame_basic/background.png")
+background = pygame.image.load("C:/pygame/arcadegame-python-pygame/pygame_basic/background.png")
 
 # 캐릭터(sprite) 불러오기
-character = pygame.image.load("C:/PythonWorkspace/pygame_basic/character.png")
+character = pygame.image.load("C:/pygame/arcadegame-python-pygame/pygame_basic/character.png")
 character_size = character.get_rect().size                      # 이미지 크기를 구해옴
 character_width = character_size[0]                             # 캐릭터의 가로크기
 character_height = character_size[1]                            # 캐릭터의 세로크기
@@ -30,6 +30,15 @@ to_y = 0
 
 # 이동 속도
 character_speed = 0.6
+
+# 적 enemy 캐릭터
+enemy = pygame.image.load("C:/pygame/arcadegame-python-pygame/pygame_basic/enemy.png")
+enemy_size = enemy.get_rect().size                      # 이미지 크기를 구해옴
+enemy_width = enemy_size[0]                             # 캐릭터의 가로크기
+enemy_height = enemy_size[1]                            # 캐릭터의 세로크기
+enemy_x_pos = (screen_width / 2) - (enemy_width / 2)                # 화면 가로의 중간위치
+enemy_y_pos = (screen_height / 2) - (enemy_height / 2)              # 화면 세로 가장 하단
+
 
 # 이벤트 루프
 running = True # 게임이 진행중인가?
@@ -72,10 +81,23 @@ while running:
     elif character_y_pos > screen_height - character_height:
         character_y_pos = screen_height - character_height
 
+    # 충돌 처리를 위한 rect 정보 업데이트
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    enemy_rect = enemy.get_rect()
+    enemy_rect.left = enemy_x_pos
+    enemy_rect.top = enemy_y_pos
+
+    # 충돌 체크
+    if character_rect.colliderect(enemy_rect):
+        print("충돌함")
+        running = False # 충돌하면 게임 종료
 
     screen.blit(background, (0, 0)) # 배경 그리기
-
-    screen.blit(character, (character_x_pos, character_y_pos))
+    screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터 그리기
+    screen.blit(enemy, (enemy_x_pos, enemy_y_pos))  # 적 그리기
 
     pygame.display.update() # 게임화면을 다시 그리기
 
--- pygame_basic/7_text.py
+++ pygame_basic/7_text.py
@@ -14,10 +14,10 @@ pygame.display.set_caption("웅겹살의 오락실게임") # 화면설정 screen
 clock = pygame.time.Clock()
 
 # 배경 이미지 불러오기
-background = pygame.image.load("C:/PythonWorkspace/pygame_basic/background.png")
+background = pygame.image.load("C:/pygame/arcadegame-python-pygame/pygame_basic/background.png")
 
 # 캐릭터(sprite) 불러오기
-character = pygame.image.load("C:/PythonWorkspace/pygame_basic/character.png")
+character = pygame.image.load("C:/pygame/arcadegame-python-pygame/pygame_basic/character.png")
 character_size = character.get_rect().size                      # 이미지 크기를 구해옴
 character_width = character_size[0]                             # 캐릭터의 가로크기
 character_height = character_size[1]                            # 캐릭터의 세로크기
@@ -30,6 +30,24 @@ to_y = 0
 
 # 이동 속도
 character_speed = 0.6
+
+# 적 enemy 캐릭터
+enemy = pygame.image.load("C:/pygame/arcadegame-python-pygame/pygame_basic/enemy.png")
+enemy_size = enemy.get_rect().size                      # 이미지 크기를 구해옴
+enemy_width = enemy_size[0]                             # 캐릭터의 가로크기
+enemy_height = enemy_size[1]                            # 캐릭터의 세로크기
+enemy_x_pos = (screen_width / 2) - (enemy_width / 2)                # 화면 가로의 중간위치
+enemy_y_pos = (screen_height / 2) - (enemy_height / 2)              # 화면 세로 가장 하단
+
+# 폰트 정의
+game_font = pygame.font.Font(None, 40) # 폰트 객체 생성 (폰트, 크기)
+
+# 총 시간
+total_time = 10
+
+# 시작 시간
+start_ticks = pygame.time.get_ticks() # 시간 tick 을 받아옴
+
 
 # 이벤트 루프
 running = True # 게임이 진행중인가?
@@ -72,12 +90,41 @@ while running:
     elif character_y_pos > screen_height - character_height:
         character_y_pos = screen_height - character_height
 
+    # 충돌 처리를 위한 rect 정보 업데이트
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    enemy_rect = enemy.get_rect()
+    enemy_rect.left = enemy_x_pos
+    enemy_rect.top = enemy_y_pos
+
+    # 충돌 체크
+    if character_rect.colliderect(enemy_rect):
+        print("충돌함")
+        running = False # 충돌하면 게임 종료
 
     screen.blit(background, (0, 0)) # 배경 그리기
+    screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터 그리기
+    screen.blit(enemy, (enemy_x_pos, enemy_y_pos))  # 적 그리기
 
-    screen.blit(character, (character_x_pos, character_y_pos))
+    # 타이머 집어 넣기
+    # 경과 시간 계산
+    elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000 # 경과시간이 밀리세컨즈(ms)이기때문에 1000으로 나눠서 초(s)로 보여줌
+
+    timer = game_font.render(str(int(total_time - elapsed_time)), True, (255, 255, 255))
+    # render뒤 정보 > 출력할 글자(시간), True, 글자 색상
+    screen.blit(timer, (10,10))
+
+    # 만약 시간이 0 이하이면 게임 종료
+    if total_time - elapsed_time <= 0:
+        print("타임아웃")
+        running = False
 
     pygame.display.update() # 게임화면을 다시 그리기
+
+# 종료 직전 잠시 대기
+pygame.time.delay(2000) # 2초 정도 대기 (ms)
 
 # pygame 종료
 pygame.quit()
